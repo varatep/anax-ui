4da2faf3b83bb96789ecb5d5e78909cc24efdc55
Varatep Buranintu <varatep@buranintu.com>
2017-07-13 14:22:44 -0700

    finish terminated agreements with filter and sorted by desc order

--- src/components/Dashboard/terminatedAgreements.js
+++ src/components/Dashboard/terminatedAgreements.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 import Pagination from 'react-js-pagination';
+import * as _ from 'lodash';
 import {
   Segment,
   Header,
@@ -15,41 +16,78 @@ require('./pagination.css');
 
 const ITEMS_PER_PAGE = 10;
 
+function arraysEqual(arr1, arr2) {
+  if(arr1.length !== arr2.length) return false;
+  for(var i = arr1.length; i--;) {
+    if(arr1[i] !== arr2[i])
+    return false;
+  }
+  return true;
+}
+
 class TerminatedAgreements extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       data: [],
+      filteredAgreements: [],
+      unfilteredAgreements: [],
       activePage: 1,
     };
 
     this.handlePageClick = this.handlePageClick.bind(this);
+    this.updateFilter = this.updateFilter.bind(this);
   }
 
   componentDidMount() {
     const pageCount = Math.ceil(this.props.agreements / ITEMS_PER_PAGE);
-    console.log('props in mount', this.props);
     let data;
     if (this.props.agreements.length >= ITEMS_PER_PAGE) {
       data = this.props.agreements.slice(0, 10);
     } else {
       data = this.props.agreements;
     }
-    console.log('sliced', data);
-    this.setState({ pageCount, data});
+    this.setState({ pageCount, data, unfilteredAgreements: this.props.agreements, filteredAgreements: this.props.agreements });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.filter !== this.props.filter || nextProps.agreements !== this.props.agreements) {
+      this.updateFilter(nextProps.filter);
+    }
+  }
+
+  updateFilter(newFilter) {
+    if (this.state.unfilteredAgreements.length === 0) return;
+
+    let newAgreements;
+    newAgreements = _.filter(this.state.unfilteredAgreements, (ag) => {
+      if (_.includes(newFilter, ag.current_agreement_id) ||
+        _.includes(ag.name, newFilter) ||
+        _.includes(ag.sensor_url,newFilter) ||
+        _.includes(ag.consumer_id,newFilter) ||
+        _.includes(ag.counterparty_address, newFilter) ||
+        _.includes(ag.proposal, newFilter) ||
+        _.includes(ag.proposal_sig, newFilter) ||
+        _.includes(ag.terminated_description, newFilter) ||
+        _.includes(ag.terminated_reason, newFilter))
+        return true;
+    });
+    let that = this;
+    this.setState({filteredAgreements: newAgreements, activePage: 1}, () => {
+      that.prepareNextPage();
+    })
   }
 
   prepareNextPage() {
     // Do client-sided pagination instead of server-sided. It would be nice
     //  to eventually implement a server-sided implementation.
-    const newState = this.props.agreements.slice(ITEMS_PER_PAGE * (this.state.activePage - 1), ITEMS_PER_PAGE * this.state.activePage + 1);
+    const newState = this.state.filteredAgreements.slice(ITEMS_PER_PAGE * (this.state.activePage - 1), ITEMS_PER_PAGE * this.state.activePage);
     this.setState({data: newState});
   }
 
   handlePageClick(pageNumber) {
     // let offset = Math.ceil(selected * this.props.perPage);
-    console.log('page clicked', pageNumber);
     this.setState({activePage: pageNumber}, () => {
       this.prepareNextPage();
     });
@@ -108,7 +146,7 @@ class TerminatedAgreements extends Component {
                   { ag.agreement_protocol_terminated_time > 0 &&
                     <List.Description><strong>Agreement Protocol Terminated Time</strong>: {prettyTime(ag.agreement_protocol_terminated_time)}</List.Description> }
                   { ag.workload_terminated_time > 0 &&
-                    <List.Description><strong>Workload Terminated Terminated Time</strong>: {prettyTime(ag.workload_terminated_time)}</List.Description> }
+                    <List.Description><strong>Workload Terminated Time</strong>: {prettyTime(ag.workload_terminated_time)}</List.Description> }
                 </div>
               </List.Content>
             </List.Item>
@@ -125,7 +163,7 @@ class TerminatedAgreements extends Component {
             <Pagination
               activePage={this.state.activePage}
               itemsCountPerPage={ITEMS_PER_PAGE}
-              totalItemsCount={this.props.agreements.length}
+              totalItemsCount={this.state.filteredAgreements.length}
               pageRangeDisplayed={5}
               onChange={this.handlePageClick}
             />
