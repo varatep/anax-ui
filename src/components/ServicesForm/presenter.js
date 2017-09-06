eb978430705e0e1b28f563c4c773c104972ef40c
Varatep Buranintu <varatep@buranintu.com>
2017-09-06 14:19:36 -0700

    input now returns a second param, so we have to check for field name to see if it is a toggle for metering

--- src/components/ServicesForm/presenter.js
+++ src/components/ServicesForm/presenter.js
@@ -104,7 +104,7 @@ class ServicesForm extends Component {
   handleFieldChange = (event, data) => {
     let segment, fieldName, targetVal;
     // if coming from checkbox, `data` will be available
-    if (typeof data === 'undefined') {
+    if (typeof data.name !== 'undefined' && !_.includes(data.name, 'metered')) {
       [segment, fieldName] = fieldSplit(event.target.name);
       targetVal = event.target.value;
     }
