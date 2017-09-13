0386901c8d24313b5b7b64f584ea293f76fc7795
Varatep Buranintu <varatep@buranintu.com>
2017-09-12 23:45:02 -0700

    make travis build successfully

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {shallow} from 'enzyme';
+import Main from './presenter';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
+  const App = shallow(
+    <div />
+  );
+
+  expect(App).not.toBeNull();
+  expect(App).not.toBeUndefined();
 });
