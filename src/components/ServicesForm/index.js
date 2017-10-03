0825cef2bc2c094bf6848ba0b493cee9f0ed06b9
Varatep Buranintu <varatep@buranintu.com>
2017-10-02 19:03:29 -0700

    make presenter aware of microservices and workloads state

--- src/components/ServicesForm/index.js
+++ src/components/ServicesForm/index.js
@@ -5,26 +5,45 @@ import * as actions from '../../actions';
 
 import * as _ from 'lodash';
 
-import ServicesForm from './presenter';
+// import ServicesForm from './presenter';
+import ServicesForm from './mswlPresenter';
 
 // takes as input the state from a reducer
-function mapStateToProps(state) {
-  // no need to do any translation here
+// function mapStateToProps(state) {
+//   // no need to do any translation here
+//   return {
+//     servicesForm: state.servicesForm,
+//     deviceForm: state.deviceForm,
+//     attributes: state.attributes,
+//     configuration: state.configuration,
+//   };
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     servicesFormFieldChange: bindActionCreators(actions.servicesFormFieldChange, dispatch),
+//     servicesFormSubmit: bindActionCreators(actions.servicesFormSubmit, dispatch),
+//     servicesFormMultiFieldChange: bindActionCreators(actions.servicesFormMultiFieldChange, dispatch),
+//     onAttributesGet: bindActionCreators(actions.attributes, dispatch)
+//   };
+// }
+
+const mapStateToProps = (state) => {
   return {
     servicesForm: state.servicesForm,
     deviceForm: state.deviceForm,
     attributes: state.attributes,
     configuration: state.configuration,
-  };
+    services: state.services,
+  }
 }
 
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
-    servicesFormFieldChange: bindActionCreators(actions.servicesFormFieldChange, dispatch),
-    servicesFormSubmit: bindActionCreators(actions.servicesFormSubmit, dispatch),
-    servicesFormMultiFieldChange: bindActionCreators(actions.servicesFormMultiFieldChange, dispatch),
-    onAttributesGet: bindActionCreators(actions.attributes, dispatch)
-  };
+    onMicroservicesGet: bindActionCreators(actions.microservices, dispatch),
+    onWorkloadsGet: bindActionCreators(actions.workloads, dispatch),
+    onConfigurationGet: bindActionCreators(actions.configuration, dispatch),
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ServicesForm);
