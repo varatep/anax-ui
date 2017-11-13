6e677a0417a50f25612893ed1f837737ba834479
Varatep Buranintu <varatep@buranintu.com>
2017-11-13 09:47:49 -0800

    set device configured successfully state

--- src/reducers/device.js
+++ src/reducers/device.js
@@ -1,12 +1,14 @@
 import * as actionTypes from '../constants/actionTypes';
 
-const initialState = {'isFetching': true};
+const initialState = {'isFetching': true, deviceConfigured: false};
 
 // responsible for merging the existing state (state) w/ the state that comes from the action (in action)
 export default function(state = initialState, action) {
   switch (action.type) {
     case actionTypes.DEVICE_SET:
       return setDevice(state, action);
+    case actionTypes.DEVICE_CONFIGURED_SUCCESS:
+      return Object.assign({}, state, {deviceConfigured: true});
     default:
       return state;
   };
