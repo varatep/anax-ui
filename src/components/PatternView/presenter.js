37d4d5e2abda4cde4a3b0b1377f5dd097b5caf05
Varatep Buranintu <varatep@buranintu.com>
2017-12-15 08:31:48 -0800

    force pattern usage to include org

--- src/components/PatternView/presenter.js
+++ src/components/PatternView/presenter.js
@@ -126,8 +126,9 @@ class PatternView extends Component {
       onSetDeviceConfigured,
     } = this.props;
     this.setState({ephemeral: {submitting: true}});
+
     // CB hell... would prefer promise-sequential
-    accountFormDataSubmit(configuration.exchange_api, accountForm.fields.account.deviceid, accountForm, true, this.state.selectedPattern.split('/')[1])
+    accountFormDataSubmit(configuration.exchange_api, accountForm.fields.account.deviceid || device.id, accountForm, true, this.state.selectedPattern)
         .then((res) => {
           // Need to wait for account form fetch to finish
           setTimeout(() => {
